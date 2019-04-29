ba52282f15cd0548e6b7fdb204d8f72b067342c3
sup.me <1035924544@qq.com>
2019-04-29 15:53:52 +0800

    第一次优化：增加computed属性代理访问，并在此基础上更改/优化

--- SelfVue.js
+++ SelfVue.js
@@ -6,18 +6,18 @@ function SelfVue(object) {
     this.vm = this; // 绑定自身作用域
     this.data = object.data; // 绑定数据
     this.methods = object.methods; //绑定方法
-    this.computed = object.computed; //绑定计算属性
+    this.computed = object.computed || ''; //绑定计算属性, || 用来避免没有写computed而产生的BUG
     // 属性访问代理,使得 实例.属性名 = 实例.data.属性名
-    Object.keys(this.data).forEach( key => this.proxykeys(key));
+    Object.keys(this.data).forEach( key => this.proxykeysData(key));
+    Object.keys(this.computed).forEach( key => this.proxykeysCMP(key));
     observe(this.data); // 遍历数据，是每个属性为响应式
     new Compile(object.el, this.vm); //绑定页面DOM元素，并且解析指令
-    computed(object.computed, this.vm);
     return this;
 }
 
 SelfVue.prototype = {
-    /* 属性访问代理 */
-    proxykeys(key) {
+    /* data属性访问代理 */
+    proxykeysData(key) {
         Object.defineProperty(this, key, {
             enumerable: false, // 不可被遍历
             configurable: true, // 可修改
@@ -28,6 +28,16 @@ SelfVue.prototype = {
             set(newValue) {
                 // 实现属性访问代理
                 this.data[key] = newValue;
+            }
+        })
+    },
+    proxykeysCMP(key) {
+        Object.defineProperty(this, key, {
+            enumerable: false, // 不可被遍历
+            configurable: true, // 可修改
+            get() {
+                // 代理访问，在访问获取对应computed属性使返回 【函数】，并绑定作用域。之后便不需要再次绑定
+                return this.computed[key].bind(this.data);
             }
         })
     }
--- Watcher.js
+++ Watcher.js
@@ -4,13 +4,12 @@
  * @param {string} key 键名,属性名
  * @param {function} callBack 更新视图的回调函数
  */
-function Watcher(vm, key, id, callBack, upviewcb) {
+function Watcher(vm, key, callBack, upviewcb) {
     this.vm = vm; // 绑定作用域,指向 selfvue 的作用域
     this.key = key; // 绑定键名
     this.cb = callBack; // 绑定回调函数
-    this.value = this.get(); //获取值
-    this.id = id;
     this.upviewcb = upviewcb;
+    this.value = this.get(); //获取值
 }
 
 Watcher.prototype = {
@@ -20,10 +19,10 @@ Watcher.prototype = {
     },
     /* 更新视图数据的函数 */
     run() {
-        if (this.id === 1) {
-            this.cb.call(this.vm, this.vm.data[this.key]);
+        if (typeof this.upviewcb === "undefined") {
+            this.cb.call(this.vm, this.vm[this.key]);
         } else {
-            let value = this.cb.bind(this.vm)(); // 获取新值
+            let value = this.cb(); // 获取新值
             let oldValue = this.value; // 获取旧值
             if (value === oldValue) return; // 值未发生改变
             this.value = value; // 更新旧值
@@ -31,16 +30,14 @@ Watcher.prototype = {
         }
     },
     get() {
-    //    Dep.target = this;
-    //    this.vm.data['name'];
-    //    Dep.target = null;
         // 执行observe的get(),使得watcher添加入deps数组中
-        if(typeof this.vm.data[this.key] === 'undefined') {
-            dep.addSub(this);
-        } else {
+        if(typeof this.upviewcb === "undefined") {
             Dep.target = this; // 绑定自身
-            this.vm.data[this.key];
+            this.vm[this.key];
             Dep.target = null; // 添加deps数组成功后释放
+        } else {
+            dep.addSub(this);
+            return this.cb();
         }
     }
 }
